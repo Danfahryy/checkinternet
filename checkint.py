b1902f5f82cb38e36a00a82263d58c83ae2c6ab3
Danfahryy <156328920+Danfahryy@users.noreply.github.com>
2024-08-21 21:25:59 +0700

    checkint.py

--- checkint.py
+++ checkint.py
@@ -18,15 +18,31 @@ def print_welcome_message():
     welcome_text = figlet.renderText('Danvertt')
     print(welcome_text)
 
+def display_menu():
+    print("Pilih opsi:")
+    print("1. Cek Koneksi Internet")
+    print("2. Keluar")
+
 def main():
     print_welcome_message()  # Tampilkan pesan selamat datang
+    
     while True:
-        if check_internet_connection():
-            print("Koneksi internet tersedia.")
+        display_menu()  # Tampilkan menu pilihan
+        choice = input("Masukkan pilihan (1/2): ").strip()
+
+        if choice == '1':
+            if check_internet_connection():
+                print("Koneksi internet tersedia.")
+            else:
+                print("Koneksi internet tidak tersedia.")
+        elif choice == '2':
+            print("Keluar dari program.")
+            break
         else:
-            print("Koneksi internet tidak tersedia.")
-        # Tunggu 60 detik sebelum pengecekan berikutnya
-        time.sleep(60)
+            print("Pilihan tidak valid. Silakan pilih 1 atau 2.")
+        
+        # Tunggu 1 detik sebelum menampilkan menu lagi
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
